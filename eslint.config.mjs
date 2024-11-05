e03658981323a89935a624fdf9a152cf3aca415d
Nicolas <nicolaslef53@gmail.com>
2024-11-05 20:02:41 +0100

    settings

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import jsdoc from 'eslint-plugin-jsdoc';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    files: ["**/*.ts"],
+    plugins: {
+      jsdoc,
+    },
+    ignores: [
+      "node_modules/*",
+      "dist/*",
+      "**./*.d.ts"
+    ],
+    rules: {
+      "no-console": "warn",
+      'jsdoc/require-description': 'warn'
+    }
+  },
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  jsdoc.configs['flat/recommended-typescript']
+);
